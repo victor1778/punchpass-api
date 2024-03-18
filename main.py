87c43f22ea3eb04d6261ba8415e2b2bd2203332c
Victor Fonseca Vega <vrfvega@victors-air-3.mynetworksettings.com>
2024-03-17 20:21:36 -0400

    added endpoint to return user data

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 from mangum import Mangum
 
-import cProfile
-import pstats
-
 from scraper import Scraper
+from user_data_fetcher import fetch_punchpass_user_data, extract_user_data
 
 app = FastAPI()
 handler = Mangum(app)
@@ -15,3 +14,9 @@ scraper = Scraper()
 async def read_schedule():
     schedule = scraper.get_schedule()
     return {"schedule": schedule}
+
+@app.post("/user/{email}")
+async def read_schedule(email: Annotated[str, Path(title="The email of Punchpass user to get")]):
+    response = fetch_punchpass_user_data(email, scraper.cookies_store)
+    data = extract_user_data(response)
+    return {"user": data}
